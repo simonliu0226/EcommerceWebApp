95043446c51fd3d0cbeb36ee2532f8cf9098f7db
sliupp <simonliu123456789@gmail.com>
2024-12-28 18:26:11 -0700

    Create Navbar.jsx

---
+++ frontend/my-ecommerce-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+function Navbar() {
+
+  return (
+    <div>
+        <div>
+            <h1>My E-Commerce App</h1>
+        </div>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+            </ul>
+        </div>
+        <div>
+            <button>Login/Register</button>
+            <button>Shopping Cart</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
